6d2967aa8183c6679acf4651c56b029032244d02
Kevin Rodriguez <kevin94rdgz@gmail.com>
2017-02-07 10:45:43 -0500

    updated with new api changes

---
+++ bytewiser/buffer-from-string.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+console.log( Buffer.from('bytewiser') );
--- bytewiser/hexadecimal encoding.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-
-var buffer = new Buffer(process.argv.slice(2));
-console.log(buffer.toString('hex'));
